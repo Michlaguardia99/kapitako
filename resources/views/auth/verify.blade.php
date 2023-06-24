7dbfec2ed7e66bdd5455bad570a799b3e49dee81
Michael Alain Laguardia <101618737+Michlaguardia99@users.noreply.github.com>
2023-06-24 20:35:34 +0800

    kapitako

---
+++ resources/views/auth/verify.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8" style="margin-top: 2%">
+                <div class="card" style="width: 40rem;">
+                    <div class="card-body">
+                        <h4 class="card-title">Verify Your Email Address</h4>
+                        @if (session('resent'))
+                            <p class="alert alert-success" role="alert">A fresh verification link has been sent to
+                                your email address</p>
+                        @endif
+                        <p class="card-text">Before proceeding, please check your email for a verification link.If you
+                            did not receive the email,</p>
+                            <a  onclick="event.preventDefault(); document.getElementById('email-form').submit();">{{ __('click here to request another') }}</a>.
+<form id="email-form" action="{{ route('verification.resend') }}" method="POST" style="display: none;">@csrf</form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
