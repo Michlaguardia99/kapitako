c9fcbb962184e61e64e87123fe569b4684aeca9f
Michael Alain Laguardia <101618737+Michlaguardia99@users.noreply.github.com>
2023-07-30 23:26:00 +0800

    Alert Fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>@yield('title') || {{ config('app.name') }}</title>
+    <title>@yield('title') {{ config('app.name') }}</title>
     <meta content="KAPI TAKO" name="author">
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.18/dist/sweetalert2.min.css">
@@ -30,6 +30,31 @@
 
         @include('layouts.footer')
     </div>
+    <!-- layout.blade.php or your main blade file -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.18/dist/sweetalert2.min.css">
+    @if(Session::has('success') && !auth()->user()->userAlerts->contains('user_id', auth()->id()))
+    <!-- SweetAlert to display the success message -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.19/dist/sweetalert2.all.min.js"></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            Swal.fire({
+                icon: 'success',
+                title: 'Success!',
+                text: "{{ Session::get('success') }}",
+            }).then(() => {
+                // Save the user alert in the database to indicate that it has been shown to the current user
+                fetch("{{ route('user.alerts.store') }}", {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'X-CSRF-TOKEN': "{{ csrf_token() }}",
+                    },
+                    body: JSON.stringify({}),
+                });
+            });
+        });
+    </script>
+    @endif
 
     @include('includes.main-js')
 </body>
