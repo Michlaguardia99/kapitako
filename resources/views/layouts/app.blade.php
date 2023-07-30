7e268ba2cdd03bbf9079f8ae5deb124b5a3e0be8
Michael Alain Laguardia <101618737+Michlaguardia99@users.noreply.github.com>
2023-07-30 14:20:32 +0800

    dead

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>@yield('title') || {{ config('app.name') }}</title>
     <meta content="KAPI TAKO" name="author">
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.18/dist/sweetalert2.min.css">
     <!-- Favicon -->
     <link rel="icon" href="{{ asset('images/favicon.png') }}">
 
@@ -13,8 +14,9 @@
 </head>
 
 <body class="c-app">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.18/dist/sweetalert2.min.css">
     @include('layouts.sidebar')
-
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.19/dist/sweetalert2.all.min.js"></script>
     <div class="c-wrapper">
         <header class="c-header c-header-light c-header-fixed">
             @include('layouts.header')
@@ -34,4 +36,5 @@
 
     @include('includes.main-js')
 </body>
+
 </html>
