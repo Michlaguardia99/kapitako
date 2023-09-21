e1c80b406667b2968a9c40a4163a7b5fb91b8be5
Michael Alain Laguardia <101618737+Michlaguardia99@users.noreply.github.com>
2023-09-21 14:27:45 +0800

    fix

--- app/Http/Middleware/EnsureEmailIsVerified.php
+++ app/Http/Middleware/EnsureEmailIsVerified.php
@@ -4,8 +4,6 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-
 
 class EnsureEmailIsVerified
 {
@@ -15,35 +13,19 @@ class EnsureEmailIsVerified
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
-     * 
      */
     public function handle($request, Closure $next)
     {
         // Check if the user is authenticated
-    if (Auth::check()) {
-        // Check if the user's email is verified
-        $user = Auth::user();
-        $isVerified = $user->hasVerifiedEmail();
-        dd($isVerified); // Add this line for debugging
-
-        if (!$isVerified) {
-            // Redirect the user to a page informing them to verify their email
-            return redirect()->route('verification.notice');
+        if (Auth::check()) {
+            // Check if the user's email is verified
+            if (!Auth::user()->hasVerifiedEmail()) {
+                // Redirect the user to a page informing them to verify their email
+                return redirect()->route('verification.notice');
+            }
         }
-    }
-
-    // If the user is authenticated and verified, or if the user is a guest, proceed to the next middleware or route handler
-    return $next($request);
-        // Check if the user is authenticated
-        // if (Auth::check()) {
-        //     // Check if the user's email is verified
-        //     if (!Auth::user()->hasVerifiedEmail()) {
-        //         // Redirect the user to a page informing them to verify their email
-        //         return redirect()->route('verification.notice');
-        //     }
-        // }
 
         // If the user is authenticated and verified, or if the user is a guest, proceed to the next middleware or route handler
-        // return $next($request);
+        return next($request);
     }
 }
