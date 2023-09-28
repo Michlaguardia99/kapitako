4b6ecf67afc59bcce997be957216298878cfc9ea
Michael Alain Laguardia <101618737+Michlaguardia99@users.noreply.github.com>
2023-09-28 11:20:04 +0800

    pdf fix

--- config/snappy.php
+++ config/snappy.php
@@ -36,7 +36,8 @@ return [
     'pdf' => [
         'enabled' => true,
         // 'binary'  => env('WKHTML_PDF_BINARY', base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),),
-        'binary'  => '"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe"',
+        // 'binary'  => '"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe"',
+        'binary'  => '/usr/bin/wkhtmltopdf', // Update this path
         'timeout' => false,
         'options' => [  
             'enable-local-file-access' => true,
@@ -47,7 +48,8 @@ return [
 
     'image' => [
         'enabled' => true,
-        'binary'  => '""C:\Program Files\wkhtmltopdf\bin\wkhtmltoimage.exe"',
+        // 'binary'  => '""C:\Program Files\wkhtmltopdf\bin\wkhtmltoimage.exe"',
+        'binary'  => '/usr/bin/wkhtmltoimage', // Update this path
         'timeout' => false,
         'options' => [
             'enable-local-file-access' => true
